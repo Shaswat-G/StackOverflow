b916516004e8f9b17373a9455f69adc2553a0c37
Shaswat Gupta <shagupta@ethz.ch>
2025-01-25 08:34:51 +0100

    Created an answer analyzer class to orchestrate connection, extraction and computation

---
+++ answer_analysis/answer_analyzer.py
@@ -0,0 +1,26 @@
+from mongo_connection import MongoDBConnection
+from answer_extractor import AnswerExtractor
+from answer_metrics import AnswerMetrics
+
+class AnswerAnalyzer:
+    
+    def __init__(self, db_config: dict, collection_name: str):
+        self.db_config = db_config
+        self.collection_name = collection_name
+        self.db_connection = MongoDBConnection(self.db_config['uri'], self.db_config['database_name'])
+        self.db_connection.connect()
+        self.answer_extractor = AnswerExtractor(self.db_connection, self.collection_name)
+        
+    def analyze(self, limit: int = 100) -> list:
+        
+        extracted_answers = self.answer_extractor.fetch_and_extract(limit)
+        
+        results = []
+        for answer in extracted_answers:
+            metrics = AnswerMetrics(answer).compute_metrics()
+            results.append(metrics)
+
+        return results
+        
+    def close_connection(self):
+        self.db_connection.close()
